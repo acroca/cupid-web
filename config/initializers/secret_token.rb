6cb73c027ebbfaa139a5bbb00914401b0d64a9ff
Albert Callarisa Roca <shark234@gmail.com>
2012-04-02 09:28:45 +0800

    cleaned project

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-CupidWeb::Application.config.secret_token = '77a3d6898c6d6b15b5bc2c994bf1da02db4bef8df3a60524eb87805fcbc3ec516178a1a33618d353fbcdf0a5e3de6634a102e1aa16598d8935a0b013c8cdf9bb'
